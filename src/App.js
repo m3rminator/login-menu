dac26974c26d61b951555a0f5a3a01e194c66744
m3rminator <merminator.toop@gmail.com>
2021-09-13 17:20:13 +0430

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+function App() {
+
+  return (
+    <>
+    <h1 id="h1">login</h1>
+    <form>
+  <input type="email" placeholder="youremail" id="email-input"></input>
+  <input type="password" placeholder="your pass" id="pass-input"></input>
+  </form>
+  </>
+    );
+}
+export default App;
